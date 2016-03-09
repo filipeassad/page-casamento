a26bcd2b39742a29a820a86d20d76ef1555058ae
Filipe <filipeassad@gmail.com>
2016-03-09 14:47:14 -0400

    Zuando a may

--- convidado/views.py
+++ convidado/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Post
 
-
-
 def list_convidados(request):
     post = Post.objects.all()
-    return render(request, 'convidado/list_convidados.html',{'posts': post})
+    return render(request, 'convidado/list_convidados.html', {'posts': post})
+def detalhe(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'convidado/detalhe.html', {'post': post})
