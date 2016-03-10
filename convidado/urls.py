aafbaa859c7e2bcd009beba6b7531b23dc760ece
Filipe <filipeassad@gmail.com>
2016-03-10 17:26:17 -0400

    Sou lindo

--- convidado/urls.py
+++ convidado/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.list_convidados),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detalhe),
+    url(r'^post/new/$', views.novo_convidado, name='novo_convidado'),
+    url(r'^mapa', views.mapa_casamento),
 ]
--- convidado/views.py
+++ convidado/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def list_convidados(request):
     post = Post.objects.all()
@@ -7,3 +9,19 @@ def list_convidados(request):
 def detalhe(request, pk):
     post = Post.objects.get(id=pk)
     return render(request, 'convidado/detalhe.html', {'post': post})
+
+def novo_convidado(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.save()
+            return redirect('convidado.views.detalhe', pk = post.id)
+
+    else:
+        form = PostForm()
+        return render(request, 'convidado/novo_convidado.html', {'form': form})
+
+def mapa_casamento(request):
+    return render(request, 'convidado/mapa_exemplo.html')
+
